d827a482070b3faa1213fd270129a3f87f4f7f99
AtikShams <atikshams8@gmail.com>
2023-08-29 23:58:58 +0600

    login page done

--- skinSightAI/home/views.py
+++ skinSightAI/home/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
 
 def landing(request):
         return render(request, 'landing.html')
 
 def login(request):
-        return render(request, 'login.html')
+       if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')  # Replace 'home' with the URL name of your home page
+        else:
+            error_message = "Invalid username or password."
+       else:
+        error_message = ""
+
+       return render(request, 'login.html', {'error_message': error_message})
