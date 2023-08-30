67e665c4ad9802d9a0bf13d46506cea855bdb4ae
SamiurRahmanDhrubo <dev.samiur@gmail.com>
2023-08-30 08:42:43 +0600

    landing page updating [static files problem occurred]

--- skinSightAI/skinSightAI/settings.py
+++ skinSightAI/skinSightAI/settings.py
@@ -117,9 +117,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, 'static')
+]
+# above code could be a problem in future
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# new added on 30th auggust
+STATICFILES_DIRS = [
+    BASE_DIR, "static"
+]
