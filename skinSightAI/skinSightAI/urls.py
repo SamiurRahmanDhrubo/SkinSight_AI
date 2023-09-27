acd545a682af56b85c15e418f73f0b99d1f03bdd
Asfi <20101115@uap-bd.edu>
2023-09-27 12:38:11 +0600

    trained the ml model and added it to the backend
    created image_classifier.py to handle the user image classifications, made html edits to show the prediction on result page and had to write logic in scan_view and result in views.py for the model and the model was added as .pkl format

--- skinSightAI/skinSightAI/urls.py
+++ skinSightAI/skinSightAI/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 import home.views
 from django.contrib.auth.views import LogoutView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -37,3 +39,6 @@ urlpatterns = [
 
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
